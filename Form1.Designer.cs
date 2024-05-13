a310182f8ba29d159344074c1184909bf577df4a
artich02 <117100185+artich02@users.noreply.github.com>
2024-05-13 10:23:10 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace lab_graph
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.fillButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(358, 357);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // fillButton
+            // 
+            this.fillButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.fillButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.fillButton.Location = new System.Drawing.Point(12, 375);
+            this.fillButton.Name = "fillButton";
+            this.fillButton.Size = new System.Drawing.Size(116, 63);
+            this.fillButton.TabIndex = 1;
+            this.fillButton.Text = "Заполнить";
+            this.fillButton.UseVisualStyleBackColor = false;
+            this.fillButton.Click += new System.EventHandler(this.fillButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.clearButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.clearButton.Location = new System.Drawing.Point(134, 375);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(116, 63);
+            this.clearButton.TabIndex = 2;
+            this.clearButton.Text = "Очистить";
+            this.clearButton.UseVisualStyleBackColor = false;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.saveButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.saveButton.Location = new System.Drawing.Point(254, 375);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(116, 63);
+            this.saveButton.TabIndex = 3;
+            this.saveButton.Text = "Сохранить";
+            this.saveButton.UseVisualStyleBackColor = false;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(382, 450);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.fillButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Рундук";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button fillButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button saveButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace lab_graph
+{
+    public partial class Form1 : Form
+    {
+        private Random random = new Random();
+        //private Image chest;
+        private Image chest1;
+        //private Image coin;
+        private Image coin1;
+        private Image coin2;
+        private Image coin3;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            chest1 = Properties.Resources.chest1; 
+            
+            coin1 = Properties.Resources.coin1;
+            coin2 = Properties.Resources.coin2;
+            coin3 = Properties.Resources.coin3;
+            pictureBox1.Image = chest1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Properties.Resources.chest1;
+        }
+
+        private void fillButton_Click(object sender, EventArgs e)
+        {
+            
+            FillChestWithCoins();
+            
+            pictureBox1.Invalidate();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            ClearChest(); 
+            pictureBox1.Invalidate();
+            chest1 = Properties.Resources.chest1;
+            pictureBox1.Image = chest1;
+            pictureBox1.Invalidate();
+        }
+
+        private void saveButton_Click_1(object sender, EventArgs e)
+        {
+            SaveImage();
+        }
+
+        private void SaveImage()
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "PNG Image|*.png|JPEG Image|*.jpg|BMP Image|*.bmp";
+                saveFileDialog.Title = "Save an Image File";
+                saveFileDialog.FileName = "ChestWithCoins";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string fileName = saveFileDialog.FileName;
+                    pictureBox1.Image.Save(fileName, System.Drawing.Imaging.ImageFormat.Png);
+                    MessageBox.Show("Image saved successfully!");
+                }
+            }
+        }
+
+        private void FillChestWithCoins()
+        {
+            Graphics g = Graphics.FromImage(pictureBox1.Image);
+
+            int coinCount = random.Next(900,1000); 
+            Image coinImage1 = Properties.Resources.coin1;
+            Image coinImage2 = Properties.Resources.coin2;
+            Image coinImage3 = Properties.Resources.coin3;
+
+            // Размещаем случайное количество монет в сундуке
+            for (int i = 0; i < coinCount; i++)
+            {
+                int x = random.Next(65, 277);
+                int y = random.Next(168, 279);
+
+                if (x > 95 && x < 247 && y > 188 && y < 259)
+                { 
+
+                    int coin_1or2 = random.Next(0, 6);
+                    if (coin_1or2 == 1 || coin_1or2 == 2)
+                    {
+                        g.DrawImage(coinImage1, new Rectangle(x, y, 38, 35));
+                    }
+                    else if (coin_1or2 == 3 || coin_1or2 == 4)
+                    {
+                        g.DrawImage(coinImage2, new Rectangle(x, y, 28, 28));
+                    }
+                    //else
+                    //{
+                    //    g.DrawImage(coinImage3, new Rectangle(x, y, 28, 28));
+                    //}
+                }
+                else
+                {
+                    int coin_1or2 = random.Next(0, 6);
+                    if (coin_1or2 == 1 || coin_1or2 == 2)
+                    {
+                        g.DrawImage(coinImage1, new Rectangle(x, y, 25, 28));
+                    }
+                    else if (coin_1or2 == 3 || coin_1or2 == 4)
+                    {
+                        g.DrawImage(coinImage2, new Rectangle(x, y , 15, 15));
+                    }
+                    else
+                    {
+                        g.DrawImage(coinImage3, new Rectangle(x, y + 5, 15, 15));
+                    }
+
+                }
+            }
+
+        }
+
+        private void ClearChest()
+        {
+            if (pictureBox1.Image != null)
+            {
+                pictureBox1.Image.Dispose();
+                pictureBox1.Image = null;
+       
+            }          
+        }   
+        
+    }
+}
